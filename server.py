bb20da414b3401dcdc7f2f7f421676833cb3ddd5
JuanR <juan.rizzato@uner.edu.ar>
2026-01-21 10:15:48 -0300

    detect template

--- server.py
+++ server.py
@@ -5,5 +5,9 @@ from flask import render_template
 def home():
     return render_template('home.html')
 
+@app.route('/detect')
+def detect():
+    return render_template('detect.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
